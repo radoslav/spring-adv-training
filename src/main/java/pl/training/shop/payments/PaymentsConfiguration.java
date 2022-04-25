a75d6f015632b3147d2cbc03ad79fa1c7975cc86
landrzejewski <landrzejewski.poczta@gmail.com>
2022-04-25 12:45:37 +0200

    update

--- src/main/java/pl/training/shop/payments/PaymentsConfiguration.java
+++ src/main/java/pl/training/shop/payments/PaymentsConfiguration.java
@@ -2,21 +2,14 @@ package pl.training.shop.payments;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import pl.training.shop.time.TimeProvider;
 
 @Configuration
 public class PaymentsConfiguration {
 
-    @Primary
     @Bean
-    public PaymentIdGenerator uuidPaymentIdGenerator() {
+    public PaymentIdGenerator paymentIdGenerator() {
         return new UuidPaymentIdGenerator();
-    }
-
-    @Bean
-    public PaymentIdGenerator fakePaymentIdGenerator() {
-        return new FakePaymentIdGenerator();
     }
 
     @Bean
@@ -34,7 +27,6 @@ public class PaymentsConfiguration {
         return new PaymentProcessor(paymentIdGenerator, paymentFeeCalculator, paymentRepository, timeProvider);
     }
 
-    // @Scope(SCOPE_SINGLETON) // default
     @Bean(initMethod = "init", destroyMethod = "destroy")
     public ConsolePaymentLogger paymentLogger() {
         return new ConsolePaymentLogger();
