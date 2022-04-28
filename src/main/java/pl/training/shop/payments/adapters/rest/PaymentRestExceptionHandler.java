586436506db73f4bdf3f984f59674a3088e7f220
landrzejewski <landrzejewski.poczta@gmail.com>
2022-04-28 09:44:03 +0200

    update

--- src/main/java/pl/training/shop/payments/adapters/rest/PaymentRestExceptionHandler.java
+++ src/main/java/pl/training/shop/payments/adapters/rest/PaymentRestExceptionHandler.java
@@ -1,6 +1,7 @@
 package pl.training.shop.payments.adapters.rest;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,8 +11,10 @@ import pl.training.shop.payments.domain.PaymentNotFoundException;
 
 import java.util.Locale;
 
+import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
+@Order(HIGHEST_PRECEDENCE)
 @ControllerAdvice("pl.training.shop.payments.adapters.rest")
 @RequiredArgsConstructor
 public class PaymentRestExceptionHandler {
