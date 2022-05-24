0072a494ec2e2f3c4b353386a4968ff9ff5c959a
landrzejewski <landrzejewski.poczta@gmail.com>
2022-05-24 10:49:08 +0200

    fix

--- payments-adapters/src/main/java/pl/training/payments/adapters/persistence/jpa/JpaPaymentRepositoryAdapter.java
+++ payments-adapters/src/main/java/pl/training/payments/adapters/persistence/jpa/JpaPaymentRepositoryAdapter.java
@@ -3,12 +3,13 @@ package pl.training.payments.adapters.persistence.jpa;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import pl.training.payments.ports.*;
 
 import java.util.Optional;
 
-@Transactional // (propagation = Propagation.MANDATORY)
+@Transactional(propagation = Propagation.MANDATORY)
 @Component
 @RequiredArgsConstructor
 public class JpaPaymentRepositoryAdapter implements PaymentReader, PaymentWriter {
