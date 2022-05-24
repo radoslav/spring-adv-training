a35692943983140fcaa93160ac15e2aff97b8108
landrzejewski <landrzejewski.poczta@gmail.com>
2022-05-24 19:50:51 +0200

    fix

---
+++ commons/src/main/java/pl/training/commons/money/Currency.java
@@ -0,0 +1,12 @@
+package pl.training.commons.money;
+
+public class Currency {
+    
+    private Currency() {
+    }
+
+    public static final String DEFAULT_CURRENCY = "PLN";
+
+    public static final String CURRENCY_SEPARATOR = " ";
+
+}
--- payments-adapters/src/main/java/pl/training/payments/adapters/persistence/jpa/JpaPersistencePaymentMapper.java
+++ payments-adapters/src/main/java/pl/training/payments/adapters/persistence/jpa/JpaPersistencePaymentMapper.java
@@ -3,21 +3,22 @@ package pl.training.payments.adapters.persistence.jpa;
 import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import pl.training.commons.money.Currency;
 import pl.training.payments.ports.Payment;
 
 import java.math.BigDecimal;
 import java.util.List;
 
-@Mapper(componentModel = "spring", imports = BigDecimal.class)
+@Mapper(componentModel = "spring", imports = {BigDecimal.class, Currency.class})
 public interface JpaPersistencePaymentMapper {
 
     String CURRENCY_SEPARATOR = " ";
 
-    @Mapping(target = "value", expression = "java(new BigDecimal(payment.getValue().split(CURRENCY_SEPARATOR)[1]))")
-    @Mapping(target = "currency", expression = "java(payment.getValue().split(CURRENCY_SEPARATOR)[0])")
+    @Mapping(target = "value", expression = "java(new BigDecimal(payment.getValue().split(Currency.CURRENCY_SEPARATOR)[1]))")
+    @Mapping(target = "currency", expression = "java(payment.getValue().split(Currency.CURRENCY_SEPARATOR)[0])")
     PaymentEntity toEntity(Payment payment);
 
-    @Mapping(target = "value", expression = "java(entity.getValue() + CURRENCY_SEPARATOR + entity.getCurrency())")
+    @Mapping(target = "value", expression = "java(entity.getValue() + Currency.CURRENCY_SEPARATOR + entity.getCurrency())")
     Payment toDomain(PaymentEntity entity);
 
 
