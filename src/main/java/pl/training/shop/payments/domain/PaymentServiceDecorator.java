f09549db9404ae9d59a9309c08ca41badafcddb8
landrzejewski <landrzejewski.poczta@gmail.com>
2022-06-06 07:50:30 +0200

    Base

--- src/main/java/pl/training/shop/payments/domain/PaymentServiceDecorator.java
+++ src/main/java/pl/training/shop/payments/domain/PaymentServiceDecorator.java
@@ -3,7 +3,6 @@ package pl.training.shop.payments.domain;
 import lombok.RequiredArgsConstructor;
 import pl.training.shop.commons.Page;
 import pl.training.shop.commons.ResultPage;
-import pl.training.shop.commons.aop.Atomic;
 import pl.training.shop.payments.ports.PaymentService;
 
 import java.util.List;
@@ -13,13 +12,11 @@ public class PaymentServiceDecorator implements PaymentService {
 
     private final PaymentService paymentService;
 
-    @Atomic
     @Override
     public Payment process(PaymentRequest paymentRequest) {
         return paymentService.process(paymentRequest);
     }
 
-    @Atomic
     public List<Payment> process(List<PaymentRequest> paymentRequests) {
         return paymentRequests.stream().map(this::process).toList();
     }
