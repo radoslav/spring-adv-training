1ebc2ebe14f845488797b9dbe12243af2cb76779
landrzejewski <landrzejewski.poczta@gmail.com>
2022-06-08 11:49:35 +0200

    a

--- search/src/main/java/pl/training/search/Application.java
+++ search/src/main/java/pl/training/search/Application.java
@@ -1,8 +1,10 @@
 package pl.training.search;
 
 import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Scheduler;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.functions.Predicate;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 import lombok.extern.java.Log;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
@@ -10,7 +12,11 @@ import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
+import javax.sound.midi.Soundbank;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 import static java.lang.Runtime.getRuntime;
 import static okhttp3.logging.HttpLoggingInterceptor.Level.BASIC;
@@ -45,22 +51,32 @@ public class Application {
                 .toObservable();
     }
 
-    private void start() {
+    private void start() throws InterruptedException {
         getRuntime().addShutdownHook(new Thread(disposables::dispose));
 
         var stream= ObservableInputStream.from(System.in)
-                .map(String::toLowerCase)
-                .flatMap(this::getRepositories);
-
+                .subscribeOn(Schedulers.io())
+                .debounce(3, TimeUnit.SECONDS)
+                // .flatMap(this::getRepositories)
+                .flatMap(query -> Observable.zip(getRepositories(query), getRepositories(query.toLowerCase()), this::combine));
 
         disposables.add(stream.subscribe(result -> log.info(result.toString()), exception -> log.warning(exception.toString())));
+        log.info("Enter query: ");
+        Thread.sleep(10_000);
     }
 
     private Predicate<String> byLength(int length) {
         return name -> name.length() > length;
     }
 
-    public static void main(String[] args) {
+    private List<String> combine(List<String> data, List<String> otherData) {
+        var result = new ArrayList<String>();
+        result.addAll(data);
+        result.addAll(otherData);
+        return result;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
         new Application().start();
     }
 
