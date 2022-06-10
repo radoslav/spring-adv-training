a13776ce4fe5e4ab6e48e62b494ccfff9e066a1f
landrzejewski <landrzejewski.poczta@gmail.com>
2022-06-10 12:35:52 +0200

    a

--- chat/src/main/resources/static/js/chat.js
+++ chat/src/main/resources/static/js/chat.js
@@ -2,11 +2,52 @@ $(() => {
 
     let stompClient = null;
 
-    function connect() {
-        const socket = new SockJS('/chat');
-        stompClient = Stomp.over(socket);
+    function updateView(isConnected) {
+        $('#username').prop('disabled', isConnected);
+        $('#connectBtn').prop('disabled', isConnected);
+        $('#disconnectBtn').prop('disabled', !isConnected);
+        $('#sendBtn').prop('disabled', !isConnected);
+        $('#message').prop('disabled', !isConnected);
+        $('#recipient').prop('disabled', !isConnected);
+        if (isConnected) {
+            $('#messages').text('');
+        }
     }
 
+    function onMessage(messageEvent) {
+        const message = JSON.parse(messageEvent.body);
+        $(`<p>${message.sender} (${message.timestamp}): ${message.text}</p>`).appendTo($('#messages'));
+    }
+
+    function onConnect() {
+        updateView(true);
+        stompClient.subscribe('/main-room', onMessage);
+    }
+
+    function connect() {
+        const user =  $('#username').val();
+        const socket = new SockJS('/chat');
+        stompClient = Stomp.over(socket);
+        stompClient.connect({user}, onConnect);
+    }
+
+    function disconnect() {
+        updateView(false);
+        stompClient.disconnect();
+    }
+
+    function send() {
+        const message = {
+            sender: $('#username').val(),
+            recipient: $('#recipient').val() || 'all',
+            text: $('#message').val()
+        };
+        stompClient.send('/ws/chat', {}, JSON.stringify(message));
+    }
+
+    updateView(false);
     $('#connectBtn').click(connect);
+    $('#disconnectBtn').click(disconnect);
+    $('#sendBtn').click(send);
 
 });
